fadbb458e795d0d9b391c6ffbb3c2a73966da7a6
Bella <98971465+Bellawareness@users.noreply.github.com>
2022-05-07 05:12:01 +0000

    added comment section to solve page

---
+++ script.js
@@ -0,0 +1,59 @@
+// document.addEventListener("DOMContentLoaded", function() {
+  // code...
+
+const submit = document.querySelector('.comment-submit');
+const commentList = document.querySelector('.comments');
+const commentInput = document.querySelector('.comment-input');
+
+
+function template(data) {
+  commentList.insertAdjacentHTML("beforeend", `
+  <div class="comment flex items-start justify-start">
+      <img class="comment-avatar" src="${data.avatar}" />
+      <div class="flex-1">
+        <h3 class="comment-author">${data.author}</h3>
+        <p class="comment-body">${data.comment}</p>
+      </div>
+    </div>
+  </div>`);
+}
+
+function appendComment (event) {
+
+  const data = {
+    avatar: "https://img.icons8.com/dotty/80/000000/pet-commands-train.png",
+    comment: commentInput.value,
+    author: "Helper"
+  };
+
+
+
+
+
+
+
+
+
+  event.preventDefault();
+  // If the value is nothing just return
+  if (commentInput.value.length < 1) return;
+
+  // Insert new template into DOM
+  template(data);
+
+  // Reset textrea value
+  commentInput.value = "";
+
+  // Save the list to localStorage
+  localStorage.setItem('commentListing', commentList.innerHTML);
+}
+
+submit.addEventListener('click', appendComment, false)
+
+// Check for saved wishlist items
+const saved = localStorage.getItem('commentListing');
+
+// If there are any saved items, update our list
+if (saved) {
+  commentList.innerHTML = saved;
+}
